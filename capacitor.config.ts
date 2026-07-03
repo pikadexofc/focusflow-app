aaa6e8b36912c60f1455b8531afcf8aa63d9a02d
Zobaid Islam Santo <pikadexofc@users.noreply.github.com>
2026-07-03 23:36:10 +0600

    fix(android): resolve launch crash by removing missing smallIcon from capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,6 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   plugins: {
     LocalNotifications: {
-      smallIcon: "ic_stat_icon_config_sample",
       iconColor: "#488AFF",
       sound: "focus_alarm.wav",
     },
